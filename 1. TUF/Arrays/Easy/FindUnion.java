4fd1279ee08db5418827b4c93078b500df284326
Atul-Raj7 <meatulraj7.com>
2025-01-29 22:43:50 +0530

    .

--- 1. TUF/Arrays/Easy/FindUnion.java
+++ 1. TUF/Arrays/Easy/FindUnion.java
@@ -4,19 +4,60 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FindUnion {
+
+    // Optimal 
+    /*
+     * TC = O(n1 + n2)
+     * SC = O(n1 + n2) only because we have to return list to print answer
+     */
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
         // add your code here
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < a.length; i++){
-            set.add(a[i]);
+        int n1 = a.length;
+        int n2 = b.length;
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        while(i < n1 && j < n2){
+            if(a[i] < b[j]){
+                if(list.size() == 0 || list.get(list.size() - 1) != a[i]){
+                    list.add(a[i]);
+                }
+                i++;
+            }
+            else if(a[i] > b[j]){
+                if(list.size() == 0 || list.get(list.size() - 1) != b[j]){
+                    list.add(b[j]);
+                }
+                j++;
+            }
+            else{
+                if(list.size() == 0 || list.get(list.size() - 1) != a[i]){
+                    list.add(a[i]);
+                }
+                i++;
+                j++;
+            }
         }
-        for(int i = 0; i < b.length; i++){
-            set.add(b[i]);
+        
+        while(i < n1){
+            if(list.size() == 0 || list.get(list.size() - 1) != a[i]){
+                    list.add(a[i]);
+                }
+                i++;
         }
-        ArrayList<Integer> list = new ArrayList<>(set);
-        Collections.sort(list);
+        
+        while(j < n2){
+            if(list.size() == 0 || list.get(list.size() - 1) != b[j]){
+                    list.add(b[j]);
+                }
+                j++;
+        }
+        
+        
         return list;
     }
+
     public static void main(String[] args){
         int[] a = {1,2,3,4,5,6};
         int[] b = {4,5,6,1,2,9};
@@ -24,3 +65,24 @@ public class FindUnion {
     }
 }
 
+
+//Bruteforce
+/*
+    TC = O(nlogn1 + nlogn2) + O(n1 + n2)
+    SC = O(n1 + n2) + O(n1 + n2)
+
+public static ArrayList<Integer> findUnion(int a[], int b[]) {
+    // add your code here
+    Set<Integer> set = new HashSet<>();
+    for(int i = 0; i < a.length; i++){
+        set.add(a[i]);
+    }
+    for(int i = 0; i < b.length; i++){
+        set.add(b[i]);
+    }
+    ArrayList<Integer> list = new ArrayList<>(set);
+    Collections.sort(list);
+    return list;
+}
+
+*/
